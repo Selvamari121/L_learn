6a9a1726910dec6f2b1a3986bda61bc51ac3e25a
Selvamari121 <114737078+Selvamari121@users.noreply.github.com>
2024-07-03 14:32:05 +0530

    Add files via upload

---
+++ scriptlearn.js
@@ -0,0 +1,32 @@
+//circumference
+
+/*const PI=3.14;
+let radius;
+let circumference;
+           
+               //radius prompt--> console
+radius=window.prompt("Enter the raadius of a circle")
+radius=Number(radius);
+circumference=2*PI*radius;
+console.log(circumference);*/
+                         
+              //radius screen
+/*document.getElementById('submit').onclick=function(){
+    radius=document.getElementById('mytext').value;
+    radius=Number(radius);
+    circumference=2*PI*radius;
+    //console.log(circumference);
+    document.getElementById('circumference').textContent=circumference;
+}*/
+
+// full name screen
+
+/*let firstname;
+let lastname;
+let fullname;
+document.getElementById('submit').onclick=function(){
+    firstname=document.getElementById('fname').value;
+    lastname=document.getElementById('lname').value;
+    fullname=firstname+" "+lastname;
+    document.getElementById('name').textContent=`welcome ${fullname} .`;
+}*/
